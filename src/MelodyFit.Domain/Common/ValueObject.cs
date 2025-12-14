c2a766741f8a31e45f1ae70da0eb4eda4e1ae548
mohsensohrabi-se <mohsen.sohrabi.se@gmail.com>
2025-12-14 11:56:31 +0330

    feat: add abstract valuobject class to Common in Domain

---
+++ src/MelodyFit.Domain/Common/ValueObject.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+
+namespace MelodyFit.Domain.Common;
+
+public abstract class ValueObject
+{
+    protected abstract IEnumerable<object?> GetEqualityComponents();
+
+    public override bool Equals(object? obj)
+    {
+        if (obj is null)
+            return false;
+
+        if (obj.GetType() != GetType())
+            return false;
+
+        var other = (ValueObject)obj;
+
+        using var thisValues = GetEqualityComponents().GetEnumerator();
+        using var otherValues = other.GetEqualityComponents().GetEnumerator();
+
+        while (thisValues.MoveNext() && otherValues.MoveNext())
+        {
+            if (thisValues.Current is null ^ otherValues.Current is null)
+                return false;
+
+            if (thisValues.Current is not null &&
+                !thisValues.Current.Equals(otherValues.Current))
+                return false;
+        }
+
+        return !thisValues.MoveNext() && !otherValues.MoveNext();
+    }
+
+    public override int GetHashCode()
+    {
+        var hash = new HashCode();
+
+        foreach (var component in GetEqualityComponents())
+        {
+            hash.Add(component);
+        }
+
+        return hash.ToHashCode();
+    }
+
+    public static bool operator ==(ValueObject? left, ValueObject? right)
+    {
+        if (left is null && right is null)
+            return true;
+
+        if (left is null || right is null)
+            return false;
+
+        return left.Equals(right);
+    }
+
+    public static bool operator !=(ValueObject? left, ValueObject? right)
+        => !(left == right);
+}
