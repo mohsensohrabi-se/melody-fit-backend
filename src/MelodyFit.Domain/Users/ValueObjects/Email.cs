a1495e1c0650feb231c28edab75d8aea16d0922e
mohsensohrabi-se <mohsen.sohrabi.se@gmail.com>
2025-12-14 12:31:09 +0330

    feat:add Email Value object to User Domain

---
+++ src/MelodyFit.Domain/Users/ValueObjects/Email.cs
@@ -0,0 +1,29 @@
+﻿using MelodyFit.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MelodyFit.Domain.Users.ValueObjects;
+
+public sealed class Email : ValueObject
+{
+    public string Value { get; }
+    private Email(string value)
+    {
+        Value = value;
+    }
+
+    public static Result<Email> Create(string value)
+    {
+        if (string.IsNullOrWhiteSpace(value))
+            return Result.Failure<Email>("Email is required");
+        if (!value.Contains("@"))
+            return Result.Failure<Email>("Invalid Email Format");
+        return Result.Success<Email>(new Email(value));
+    }
+
+    protected override IEnumerable<object?> GetEqualityComponents()
+    {
+        yield return Value;
+    }
+}
