3cfbfcde997f7dfd83df3dc0c70115d3dd684afd
mohsensohrabi-se <mohsen.sohrabi.se@gmail.com>
2025-12-14 09:01:29 +0330

    feat: add DomainEvent to Common in Domain layer

---
+++ src/MelodyFit.Domain/Common/DomainEvent.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MelodyFit.Domain.Common;
+
+public abstract  record DomainEvent(Guid EventId, DateTime OccuredOn)
+{
+    protected DomainEvent() : this(Guid.NewGuid(), DateTime.UtcNow) { }
+}
