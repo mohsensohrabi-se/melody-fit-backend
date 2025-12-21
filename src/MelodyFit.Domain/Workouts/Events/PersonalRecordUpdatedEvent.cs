b1ec12fbfa89a880192879af0facd3f06c9135b9
mohsensohrabi-se <mohsen.sohrabi.se@gmail.com>
2025-12-21 14:33:54 +0330

    feat: add PersonalRecordUpdatedEvent

---
+++ src/MelodyFit.Domain/Workouts/Events/PersonalRecordUpdatedEvent.cs
@@ -0,0 +1,10 @@
+﻿using MelodyFit.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MelodyFit.Domain.Workouts.Events
+{
+    public sealed record PersonalRecordUpdatedEvent(Guid UserId, DateOnly Date):DomainEvent;
+    
+}
