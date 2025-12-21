58abd97d17d8c1365dab4dacac16aa414b45f0bd
mohsensohrabi-se <mohsen.sohrabi.se@gmail.com>
2025-12-21 14:32:43 +0330

    feat: Add PersonalRecords to the workout in Domain layer

---
+++ src/MelodyFit.Domain/Workouts/Entities/PersonalRecords.cs
@@ -0,0 +1,54 @@
+﻿using MelodyFit.Domain.Common;
+
+
+namespace MelodyFit.Domain.Workouts.Entities
+{
+    public class PersonalRecords: BaseEntity
+    {
+        public WorkoutDuration MaxWorkoutDuration { get; private set; }
+        public int MaxBurnedCalories { get; private set; }
+        public int MaxSteps { get; private set; }
+        public double LongestDistanceKm { get; private set; }
+
+        private PersonalRecords() 
+        {
+            MaxWorkoutDuration = WorkoutDuration.Zero;
+            MaxBurnedCalories = 0;
+            MaxSteps = 0;
+            LongestDistanceKm = 0;
+        }
+
+        public static PersonalRecords Create()
+            => new PersonalRecords();
+
+        public bool TryUpdate(WorkoutSummary summary)
+        {
+            bool isNewRecord = false;
+
+            if (summary.Duration.Value > MaxWorkoutDuration.Value)
+            {
+                MaxWorkoutDuration = summary.Duration;
+                isNewRecord = true;
+            }
+
+            if (summary.Calories > MaxBurnedCalories)
+            {
+                MaxBurnedCalories = summary.Calories;
+                isNewRecord = true;
+            }
+
+            if (summary.Steps > MaxSteps)
+            {
+                MaxSteps = summary.Steps;
+                isNewRecord = true;
+            }
+
+            if (summary.DistanceKm > LongestDistanceKm)
+            {
+                LongestDistanceKm = summary.DistanceKm;
+                isNewRecord = true;
+            }
+
+            return isNewRecord;
+        }
+}
