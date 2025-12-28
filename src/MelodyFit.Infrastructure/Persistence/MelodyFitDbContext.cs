7466a7cd96f6e4a695303acaca4f8af22dee508a
mohsensohrabi-se <mohsen.sohrabi.se@gmail.com>
2025-12-28 15:22:25 +0330

    fix: provide default for CancelationToken in SaveChangesAsync override

--- src/MelodyFit.Infrastructure/Persistence/MelodyFitDbContext.cs
+++ src/MelodyFit.Infrastructure/Persistence/MelodyFitDbContext.cs
@@ -30,7 +30,7 @@ namespace MelodyFit.Infrastructure.Persistence
             base.OnModelCreating(modelBuilder);
         }
 
-        public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken)
+        public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken=default)
         {
             var entitiesWithEvents = ChangeTracker
                 .Entries<BaseEntity>()
