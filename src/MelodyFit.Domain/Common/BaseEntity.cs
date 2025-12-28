02875dbba26a2ac62dea0072e8e3f3694695d365
mohsensohrabi-se <mohsen.sohrabi.se@gmail.com>
2025-12-28 15:24:53 +0330

    fix: make _domainEvents list not mapped so that EF Core do not care about them

--- src/MelodyFit.Domain/Common/BaseEntity.cs
+++ src/MelodyFit.Domain/Common/BaseEntity.cs
@@ -1,13 +1,17 @@
 ﻿
 
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace MelodyFit.Domain.Common;
 
 public abstract class BaseEntity
 {
     public Guid Id { get;  protected set; } = Guid.NewGuid();
+    [NotMapped]
     private readonly List<DomainEvent> _domainEvents = new();
+    [NotMapped]
     public IReadOnlyList<DomainEvent> DomainEvents => _domainEvents.AsReadOnly();
-
+   
     protected void AddDomainEvent(DomainEvent domainEvent)
     {
         _domainEvents.Add(domainEvent); 
