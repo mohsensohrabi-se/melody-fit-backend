a3acde971ec2e829add3d4f275860eedcfd06852
mohsensohrabi-se <mohsen.sohrabi.se@gmail.com>
2025-12-28 12:27:59 +0330

    feat: add PasswordService and register it in Program.cs

--- src/MelodyFit.API/Program.cs
+++ src/MelodyFit.API/Program.cs
@@ -1,8 +1,37 @@
 using MelodyFit.API.Middleware;
+using MelodyFit.Application.Common.Interfaces.Messaging;
+using MelodyFit.Application.Common.Interfaces.Persistence;
 using MelodyFit.Application.Common.Interfaces.Services;
+using MelodyFit.Infrastructure.Messaging;
+using MelodyFit.Infrastructure.Persistence;
+using MelodyFit.Infrastructure.Persistence.Repositories;
 using MelodyFit.Infrastructure.Services.Email;
+using MelodyFit.Infrastructure.Services.Security;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// Register Db Context + PostgreSQL
+builder.Services.AddDbContext<MelodyFitDbContext>(options =>
+{
+    options.UseNpgsql(
+        builder.Configuration.GetConnectionString("DefaultConnection"),
+        npgsql =>
+        {
+            npgsql.MigrationsAssembly("MelodyFit.Infrastructure");
+        });
+});
+
+// Register domain event dispatcher
+builder.Services.AddScoped<IDomainEventDispatcher, DomainEventDispatcher>();
+
+// Register Repositories 
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+
+
+//Register Services
+builder.Services.AddScoped<IPasswordService,PasswordService >();
 
 if (builder.Environment.IsDevelopment())
 {
@@ -12,6 +41,11 @@ else
 {
     builder.Services.AddScoped<IEmailService, SmtpEmailService>();
 }
+
+// Register MediatR 
+builder.Services.AddMediatR(cfg=>
+    cfg.RegisterServicesFromAssembly(MelodyFit.Application.AssemblyReference.Assembly)
+);
 
 // Add services to the container.
 
---
+++ src/MelodyFit.Infrastructure/Services/Security/PasswordService.cs
@@ -0,0 +1,25 @@
+﻿using MelodyFit.Application.Common.Interfaces.Services;
+
+
+namespace MelodyFit.Infrastructure.Services.Security
+{
+    public sealed class PasswordService : IPasswordService
+    {
+        public Task<string> HashPassword(string password)
+        {
+            if(string.IsNullOrWhiteSpace(password))
+                throw new ArgumentNullException("Password is empty");
+            var hash = BCrypt.Net.BCrypt.HashPassword(
+                password,
+                workFactor:12
+                );   
+            return Task.FromResult( hash );
+        }
+
+        public bool Verify(string password, string hash)
+        {
+            var result = BCrypt.Net.BCrypt.Verify(password, hash);
+            return result;
+        }
+    }
+}
